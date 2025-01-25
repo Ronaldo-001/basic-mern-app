371777a4621c5c5a07d2ee032d2e2fe442d39b02
Ronaldo-001 <ronaldop1705@gmail.com>
2025-01-25 19:22:46 +0000

    optimised Dockerfile

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-require("dotenv").config()
-const port = process.env.PORT || 5000
-app.use(cors())
-app.use(express.json())
-app.use(require("./routes/record"))
-const dbo = require("./db/conn")
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./.env" });
+const port = process.env.PORT || 5000;
 
-app.get("/", function(req, res) {
-    res.send("App is running")
-})
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+const dbo = require("./db/conn");
+
+// Serve the React build folder
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+// Catch-all route for React
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 dbo.connectToMongoDB(function (error) {
-    if (error) throw error
+  if (error) throw error;
 
-    app.listen(port, () => {
-        console.log("Server is running on port: " + port)
-    })
-})
+  app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+  });
+});
